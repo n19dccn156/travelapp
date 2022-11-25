2bfca5fe395623aa9c1be1e340ece04576b34322
ducmanhlai <ducmanhlai22@gmail.com>
2022-11-25 16:39:14 +0700

    fd

--- src/features/order-food/components/header.js
+++ src/features/order-food/components/header.js
@@ -1,33 +1,47 @@
-import React from 'react';
-import SearchComponent from './search';
-import {View, TouchableOpacity, Image, Text} from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import {styleIcon, styleText} from '../styles/styles-header';
-export function HeaderComponnent({
-  navigation,
-  nameScreen,
-  minHeight
-}: {
-  navigation: any;
-  nameScreen: string;
-  minHeight: number
-}) {
+import React from "react";
+import SearchComponent from "./search";
+import { View, TouchableOpacity, Image, Text } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { styleIcon, styleText } from "../styles/styles-header";
+import { ScrollView } from "react-native-gesture-handler";
+const options = ["Danh mục",'Cửa hàng',];
+export function HeaderComponnent({ navigation, nameScreen, minHeight }) {
   return (
-    <View style={{flex: 1, minHeight: minHeight, paddingBottom: 10}}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+    <View style={{ flex: 1, minHeight: minHeight, paddingBottom: 10 }}>
+      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
         <TouchableOpacity
           style={styleIcon.icon_close}
           onPress={() => {
             navigation.navigate(nameScreen);
-          }}>
-          <Ionicons name="chevron-back-outline" size={30} style={{color:'red'}} />
+          }}
+        >
+          <Ionicons
+            name="chevron-back-outline"
+            size={30}
+            style={{ color: "red" }}
+          />
         </TouchableOpacity>
         <Text style={styleText.header}>ĐẶT MÓN</Text>
         <TouchableOpacity style={styleIcon.icon_cart} onPress={() => {}}>
-          <Ionicons name="cart-outline" size={30} style={{color:'black'}}/>
+          <Ionicons name="cart-outline" size={30} style={{ color: "black" }} />
         </TouchableOpacity>
       </View>
       <SearchComponent />
+      <View>
+        <ScrollView  horizontal={true}>
+          <View>
+            <TouchableOpacity style={{borderColor:'black',borderWidth:1,borderRadius:5}}>
+              <View style={{ flexDirection: "row"}}>
+                <Text>Sắp xếp </Text>
+                <Text>></Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <View>
+            <Text>Lọc</Text>
+          </View>
+        </ScrollView>
+      </View>
     </View>
   );
 }
