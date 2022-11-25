3b89d843888f486a793083b46bf1f94483b221df
ducmanhlai <ducmanhlai22@gmail.com>
2022-11-26 00:22:38 +0700

    update header home food

--- src/features/food/pages/home-screen.js
+++ src/features/food/pages/home-screen.js
@@ -5,15 +5,9 @@ import LoadComponent from "../../../utility/load-component";
 import { HeaderComponnent } from "../components/header";
 import { ListShopFood } from "../components/list-shop-food";
 import { ShopFoodScreen } from "./shop-food-screen";
-<<<<<<< HEAD:src/features/order-food/pages/home-screen.js
 import { ListDish } from "../components/all-dishes";
 import { TestScreen } from "./test-redux";
-export function HomeScreen({navigation} ) {
-=======
-
-export function FoodScreen({navigation}) {
-
->>>>>>> 49912b63fbcba4f153d511fa8014fc5c6bbd3a14:src/features/food/pages/home-screen.js
+export function FoodScreen({navigation} ) {
   return (
     <View>
       <StatusBar
@@ -28,15 +22,10 @@ export function FoodScreen({navigation}) {
           nameScreen={"Home"}
           minHeight={100}
         />
-<<<<<<< HEAD:src/features/order-food/pages/home-screen.js
         {/* <ScrollView style={{ height: "100%", width: "100%", zIndex: 1 }}>
           <SafeAreaView>
-=======
-        <ScrollView style={{ height: "100%", width: "100%", zIndex: 1 }}>
-          {/* <SafeAreaView>
->>>>>>> 49912b63fbcba4f153d511fa8014fc5c6bbd3a14:src/features/food/pages/home-screen.js
             <StatusBar />
-          </SafeAreaView> */}
+          </SafeAreaView>
           <View>
             <View style={{display:'flex', flexDirection:'row', width:'100%',justifyContent:'space-between'}}>
               <Text>Nhà hàng nổi bật</Text>
@@ -44,19 +33,7 @@ export function FoodScreen({navigation}) {
             </View>
             <ListShopFood navigation={navigation} />
           </View>
-          <View style={{ flex: 1.2, width: "100%" }}>
-            <Text>Được đánh giá cao</Text>
-            <ListShopFood navigation={navigation} />
-          </View>
-          <View style={{ flex: 1.2, width: "100%" }}>
-            <Text>Được đánh giá cao</Text>
-            <ListShopFood navigation={navigation} />
-          </View>
-          <View style={{ flex: 1.2, width: "100%" }}>
-            <Text>Được đánh giá cao</Text>
-            <ListShopFood navigation={navigation} />
-          </View>
-        </ScrollView> */}
+        </ScrollView>  */}
       </SafeAreaView>
     </View>
   );
