eeb763794438f109e0f8018011e93928cc1099ca
PhanVanHieu-Ptit <96578636+HieuPit@users.noreply.github.com>
2022-12-04 16:09:41 +0700

    4h09' 04/12/2022

--- src/features/service/services/Order/postData.js
+++ src/features/service/services/Order/postData.js
@@ -1,8 +1,23 @@
 import uuid from 'react-native-uuid';
 import { variables } from '../../../../common/constants/const';
+import moment from 'moment';
 var host = variables.host;
 
 const orderService = async (idSchedule, dateStart, number, phone, service) => {
+    console.log(
+        'orderService: ',
+        'idSchedule: ' +
+            idSchedule +
+            ' dateStart: ' +
+            moment(dateStart).format('YYYY-MM-DD') +
+            '\nnumber: ' +
+            number +
+            '\nphone: ' +
+            phone +
+            '\nservice.price: ' +
+            service,
+        '\nservice.id: ' + service.id,
+    );
     try {
         const response = await fetch(`${host}/api/v1/orderservice`, {
             method: 'POST',
@@ -12,11 +27,11 @@ const orderService = async (idSchedule, dateStart, number, phone, service) => {
             },
             body: JSON.stringify({
                 id: uuid.v4(),
-                idUser: '7055dcb1-67ce-4c5f-bf51-03863f7e5778',
+                idUser: '7055dcb1-67ce-4c5f-bf51-03863f7e5779',
                 idState: 'XACNHAN',
                 idSchedule: idSchedule,
-                dateNow: new Date(),
-                dateStart: dateStart,
+                dateNow: moment().format('YYYY-MM-DD hh:mm:ss.sss'),
+                dateStart: moment(dateStart).format('YYYY-MM-DD'),
                 number: number,
                 price: service.price,
                 phone: phone,
