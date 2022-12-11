2b5cb02cbddfe2d1e8b01b79a8f69be78965cc05
PhanVanHieu-Ptit <96578636+HieuPit@users.noreply.github.com>
2022-12-11 22:53:25 +0700

    10h 53' 11/12/2022

--- src/navigations/tabbar/home-stack.tsx
+++ src/navigations/tabbar/home-stack.tsx
@@ -9,18 +9,31 @@ import { HotelStackNavigator } from '../hotel/hotel-main-stack';
 import { NotificationStackNavigator } from '../notification/notification-main-stack';
 import HomeServiceScreen from '../../features/service/views/Home/HomeServiceScreen';
 import { FoodNavigation } from '../../features/food/pages/food-navigation';
+import { MainStackNavigator } from '../../features/service/navigations/StackNavigation';
 
 const HomeStack = createNativeStackNavigator();
 
 export function HomeStackNavigator() {
     return (
         <HomeStack.Navigator>
-            <HomeStack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown: false, animation: "none"}}/>
-            <HomeStack.Screen name="ServiceScreen" component={HomeServiceScreen} options={{headerTitle: "Dịch vụ", headerTitleAlign: "center"}}/>
-            <HomeStack.Screen name="HotelStack" component={HotelStackNavigator} options={{headerShown: false}}/>
-            <HomeStack.Screen name="FoodStack" component={FoodNavigation} options={{headerShown: false}}/>
-            <HomeStack.Screen name="DiscoveryStack" component={DiscoveryStackNavigator} options={{headerShown: false}}/>
-            <HomeStack.Screen name="NotificationStack" component={NotificationStackNavigator} options={{headerShown: false}}/>
+            <HomeStack.Screen
+                name="HomeScreen"
+                component={HomeScreen}
+                options={{ headerShown: false, animation: 'none' }}
+            />
+            <HomeStack.Screen name="ServiceScreen" component={MainStackNavigator} options={{ headerShown: false }} />
+            <HomeStack.Screen name="HotelStack" component={HotelStackNavigator} options={{ headerShown: false }} />
+            <HomeStack.Screen name="FoodStack" component={FoodNavigation} options={{ headerShown: false }} />
+            <HomeStack.Screen
+                name="DiscoveryStack"
+                component={DiscoveryStackNavigator}
+                options={{ headerShown: false }}
+            />
+            <HomeStack.Screen
+                name="NotificationStack"
+                component={NotificationStackNavigator}
+                options={{ headerShown: false }}
+            />
         </HomeStack.Navigator>
     );
 }
